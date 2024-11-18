1ddfd3f0c9c7eefcdcebef0eacd7f2a20548ca68
christopheraugurius <christopher.augurius@gmail.com>
2024-11-18 17:40:04 +0700

    terbaru ceb

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Support\Facades\Log;
-use App\Models\BarangJualDetail;
 use App\Models\OrderDetail;
 use App\Models\Product;
 use App\Models\BarangJual;
@@ -18,136 +16,59 @@ class OrderController extends Controller
     {
         $order = Order::get();
         $barang = BarangJual::all();
-        $bahan = Product::all();
 
         $data = [
             'orders' => $order,
             'barang_juals' => $barang,
-            'products' => $bahan,
         ];
         return view('admin.order', $data);
     }
-
-    public function storeRequest(Request $request)
-    {
-        $data = $request->all();
-
-        // Decode the JSON string back into an array
-        if (isset($data['products'])) {
-            $data['products'] = json_decode($data['products'], true);
-        }
-
-        // Validate the request
-        $validator = Validator::make($data, [
-            'title' => 'required|string|max:255',
-            'total_price' => 'required|numeric',
-            'desc' => 'required|string',
-            'products' => 'required|array',
-            'products.*.name' => 'required|string|max:255',
-            'products.*.quantity' => 'required|integer|min:1',
-            // Add other validation rules as needed
-        ], [
-            'title.required' => 'Title is required.',
-            'total_price.required' => 'Total price is required.',
-            'desc.required' => 'Description is required.',
-            'products.required' => 'Products are required.',
-            'products.*.name.required' => 'Product name is required.',
-            'products.*.quantity.required' => 'Product quantity is required.',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['message' => $validator->errors()->first(), 'error' => true]);
-        }
-
-        DB::beginTransaction();
-        try {
-            $order = Order::create($data);
-
-            if ($request->hasFile('image')) {
-                $file = $request->file('image');
-                $path = 'public/uploads/request';
-                $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                $extension = $file->getClientOriginalExtension();
-                $fileNameToStore = $originalName . '_' . time() . '_' . uniqid() . '.' . $extension;
-                $file->storePubliclyAs($path, $fileNameToStore);
-
-                $finalData = [
-                    'name' => $data['title'],
-                    'price' => $data['total_price'] ?? 0,
-                    'stock' => 1,
-                    'description' => $data['desc'],
-                    'tipe' => 2,
-                    'image' => $fileNameToStore,
-                ];
-
-                $barangterjual = BarangJual::create($finalData);
-                OrderDetail::create([
-                    'order_id' => $order->id,
-                    'barangjual_id' => $barangterjual->id,
-                    'quantity' => 1,
-                    'price' => $order->total_price,
-                ]);
-
-                // Process products
-                foreach ($data['products'] as $productsData) {
-                    $bahan = Product::where('name', $productsData['name'])->first();
-                    if ($bahan) {
-                        BarangJualDetail::create([
-                            'barang_jual_id' => $barangterjual->id,
-                            'product_id' => $bahan->id,
-                        ]);
-                    } else {
-                        return response()->json(['message' => 'Product not found', 'error' => true]);
-                    }
-                }
-            }
-
-            DB::commit();
-            return response()->json(['message' => 'Data successfully stored', 'success' => true]);
-        } catch (\Exception $e) {
-            DB::rollBack();
-            return response()->json(['message' => 'Failed to store data', 'error' => true, 'details' => $e->getMessage()]);
-        }
-    }
-
     public function store(Request $request)
     {
-        // dd($request->all());
+        // dd(session('cart'));
+        // $cart = session('cart');
         $data = $request->all();
-        $products = $request->get('products');
+        $products = session('cart');
         $validator = Validator::make(
             $data,
             [
                 'customer_name' => 'required|string',
                 'customer_wa' => 'required|string',
                 'address' => 'required|string',
-                'title' => 'nullable|string',
-                'order_date' => 'required|date',
-                'total_price' => 'required|integer',
+                'judul_pesan' => 'nullable|string',
+                // 'order_date' => 'required|date',
+                // 'total_price' => 'required|integer',
                 'desc' => 'nullable|string',
-                'products' => 'required|array',
-                'products.*.name' => 'required|string',
-                'products.*.quantity' => 'required|integer',
-                'products.*.price' => 'required|integer',
+                // 'products' => 'required|array',
+                // 'products.*.name' => 'nullable|string',
+                // 'products.*.quantity' => 'required|integer',
+                // 'products.*.price' => 'required|integer',
             ],
             [
                 'customer_name.required' => 'Name is required.',
-                'order_date.required' => 'Order date is required.',
+                // 'order_date.required' => 'Order date is required.',
                 'customer_wa.required' => 'Customer WA is required.',
                 'address.required' => 'Customer Address is required.',
-                'title.required' => 'Judul Pesan is required.',
+                'judul_pesan.required' => 'Judul Pesan is required.',
                 'total_price.required' => 'Total Price is required.',
                 'desc.required' => 'Description is required.',
-                'products.required' => 'Products are required.',
-                'products.*.name.required' => 'Product name is required.',
-                'products.*.quantity.required' => 'Product quantity is required.',
-                'products.*.price.required' => 'Product price is required.',
+                // 'products.required' => 'Products are required.',
+                // 'products.*.name.required' => 'Product name is required.',
+                // 'products.*.quantity.required' => 'Product quantity is required.',
+                // 'products.*.price.required' => 'Product price is required.',
             ],
         );
 
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->first(), 'error' => true]);
         }
+
+        $totalPrice = 0;
+        foreach ($products as $product) {
+            $totalPrice += $product['quantity'] * $product['price'];
+        }
+        $data['total_price'] = $totalPrice;
+        $data['order_date'] = today();
         DB::beginTransaction();
         try {
             $order = Order::create($data);
@@ -173,6 +94,7 @@ class OrderController extends Controller
                 ]);
             }
             DB::commit();
+            session()->forget('cart');
             return response()->json(['message' => 'Data successfully stored', 'success' => true]);
         } catch (\Exception $e) {
             DB::rollBack();
@@ -186,36 +108,23 @@ class OrderController extends Controller
             ->where('order_id', $order->id)
             ->get();
         $barang = BarangJual::all();
+
         $data = [
             'nama' => $order,
             'order' => $detail,
             'barang_juals' => $barang,
-        ];        
-        // $notEqualToOneCount = 0;
-        // foreach ($detail as $value) {
-        //     if ($value->status != 1) {
-        //         $notEqualToOneCount++;
-        //     }
-        // }
-        // if ($notEqualToOneCount === count($detail)) {
-        //     return view('admin.order');
-        // }
+        ];
+        // dd('halo');
         return view('admin.detail_order', $data);
     }
 
     public function declineOrder(OrderDetail $order)
     {
-        $order->update(['status' => 0]);
-    
-        // $parentOrder = $order->order; 
-        // dd($parentOrder);
-        // if ($parentOrder->details->every(fn($detail) => $detail->status == 0)) {
-        //     $parentOrder->delete();
-        // }
-    
-        return response()->json(['message' => 'Order successfully declined', 'success' => true]);
+        $order->update([
+            'status' => 0,
+        ]);
+        return response()->json(['message' => 'Order successfully accepted', 'success' => true]);
     }
-    
     public function acceptOrder(OrderDetail $order)
     {
         $order->update([
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BarangJualController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\OrderController;
-use App\Http\Controllers\BarangJualController;
 use App\Http\Controllers\ExpenditureController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
@@ -24,18 +24,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/home', function() {
     return view('user.home');
-})->name('user.home');
+})->name('home');
 
-Route::get('/add-to-cart/{id}', [BarangJualController::class, 'addToCart'])->name('add-cart');
-Route::get('/cart', [BarangJualController::class, 'viewCart'])->name('cart');
-Route::get('/delete-from-cart/{id}', [BarangJualController::class, 'deleteFromCart'])->name('delete-from-cart');
-Route::get('/barang', [BarangJualController::class, 'viewCatalog'])->name('milih-barang');
-Route::get('/detail-barang/{barang}', [BarangJualController::class, 'viewDetail'])->name('detail-barang');
 Route::get('admin/login', function () {
     return view('admin.login.login');
 });
+
+Route::get('/barang', [BarangJualController::class, 'viewCatalog'])->name('milih-barang');
+Route::get('/detail-barang/{barang}', [BarangJualController::class, 'viewDetail'])->name('detail-barang');
+
+Route::get('/detail-payment', function () {
+    return view('user.detail-payment');
+})->name('detail-payment');
+
+Route::get('/form-katalog', function () {
+    return view('user.form-katalog');
+})->name('form-katalog');
+Route::post('/', [OrderController::class, 'store'])->name('katalog.store');
+
+Route::get('/add-to-cart/{id}', [BarangJualController::class, 'addToCart'])->name('add-cart');
+Route::get('/cart', [BarangJualController::class, 'viewCart'])->name('cart');
+
+Route::get('/delete-from-cart/{id}', [BarangJualController::class, 'deleteFromCart'])->name('delete-from-cart');
 
 Route::prefix('admin')->group(function () {
     Route::prefix('kategori')->group(function () {
@@ -59,7 +72,6 @@ Route::prefix('admin')->group(function () {
 
     Route::prefix('/order')->group(function () {
         Route::get('/', [OrderController::class, 'viewOrder'])->name('viewOrder');
-        Route::post('/req', [OrderController::class, 'storeRequest'])->name('order.request');
         Route::post('/', [OrderController::class, 'store'])->name('order.store');
         Route::get('/detail/{order}', [OrderController::class, 'detailOrder'])->name('order.detail');
         Route::put('/acceptOrder/{order}', [OrderController::class, 'acceptOrder'])->name('acceptOrder');
@@ -69,11 +81,9 @@ Route::prefix('admin')->group(function () {
 
     Route::prefix('purchasing')->group(function () {
         Route::get('/', [PurchaseController::class, 'index'])->name('purchase.index');
-        Route::get('/products-by-supplier/{supplier_id}', [PurchaseController::class, 'getProductsBySupplier']);
         Route::post('/store', [PurchaseController::class, 'store'])->name('purchase.store');
         Route::delete('/delete/{purchase:id}', [PurchaseController::class, 'delete'])->name('purchase.delete');
         Route::put('/update/{purchase:id}', [PurchaseController::class, 'update'])->name('purchase.update');
-        Route::put('/accept/{purchase:id}', [PurchaseController::class, 'accept'])->name('purchase.accept');
     });
 
     Route::prefix('catalog')->group(function () {
