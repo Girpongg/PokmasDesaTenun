7e4c688dbaffa5deee34baf622996bbeb9b8e816
Girpongg <c14220167@john.petra.ac.id>
2024-11-21 08:56:46 +0700

    feat: bukti transfer

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -164,7 +164,11 @@
                         <a href="{{ route('order.detail', $item->id) }}">
                             <button class="bg-black text-white w-full rounded-lg py-2 my-1">Details</button>
                         </a>
-                        <button class="bg-warning text-white w-full rounded-lg py-2 my-1">Bukti Validasi</button>
+                        <button
+                            class="bg-warning text-white w-full rounded-lg py-2 my-1"
+                            onclick='viewDetails({{$item}})'>
+                            Bukti Transfer
+                        </button>
                         <button class="bg-success text-white w-full rounded-lg py-2 my-1"
                             onclick="isvalidate({{ $item->id }})">Validasi</button>
                     </div>
@@ -180,7 +184,6 @@
             </div>
         </div>
     </div>
-
 
     <!-- Choose Barang Modal -->
     <div id="chooseBarangModal" class="hidden fixed inset-0 z-[55] bg-black bg-opacity-50 flex items-center justify-center">
@@ -543,9 +546,61 @@
         </div>
     </div>
 
-    <!-- Request Modal -->
+    <div data-te-modal-init
+        class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        id="modalDetail" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div data-te-modal-dialog-ref
+            class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+            <div
+                class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+                <div
+                    class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                    <!--Modal title-->
+                    <div class="flex items-center">
+                        <h5 class="mr-2 font-bold text-xl">Pembeli: </h5>
+                        <p id="modal-nama" class="font-bold text-lg"></p>
+                    </div>
+                    <!--Close button-->
+                    <button type="button"
+                        class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                        data-te-modal-dismiss aria-label="Close">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="h-6 w-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <!--Modal body-->
+                <div class="relative flex-auto p-4" data-te-modal-body-ref>
+
+                    <div class="flex items-center">
+                        <h5 class="font-bold">Bukti Transfer : </h5>
+                        <p class="ml-3" id="modal-order"></p>
+                    </div>
+                    {{-- ketua --}}
+                </div>
+                <!--Modal footer-->
+                <div
+                    class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                    <button type="button"
+                        class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                        data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script>
+        function viewDetails(item) {
+            console.log(item);
+            const modal = $('#modalDetail');
+            $('#modal-nama').text(item.customer_name);
+            $('#modal-order').text(item.link_bukti_tf);
+            modal.modal('show');
+        }
+
         function isvalidate(id) {
             console.log(id);
             var url_update = "{{ route('validateOrder', ':id') }}".replace(':id', id);
