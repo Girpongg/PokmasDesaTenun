98214edaa8645ace94a0d71f2958c95a97127a85
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-12-09 16:00:52 +0700

    fix: seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(OrderSeeder::class);
         $this->call(ExpanditureSeeder::class);
         $this->call(CustomerSeeder::class);
-
+        $this->call(AdminSeeder::class);
     }
 }
